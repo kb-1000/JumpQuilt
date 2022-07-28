fad14a874228c812a8e08bbb2ca72c4c9cb04abe
kb1000 <kaeptmblaubaer1000@gmail.com>
2022-07-28 14:56:02 +0200

    Bump version to 2.3.0

--- src/main/java/link/infra/jumploader/JumpQuilt.java
+++ src/main/java/link/infra/jumploader/JumpQuilt.java
@@ -34,7 +34,7 @@ import java.util.List;
 import java.util.Set;
 
 public class JumpQuilt implements ITransformationService {
-	public static final String VERSION = "2.2.0";
+	public static final String VERSION = "2.3.0";
 	public static final String USER_AGENT = "JumpQuilt/" + VERSION;
 
 	private final Logger LOGGER = LogManager.getLogger();
